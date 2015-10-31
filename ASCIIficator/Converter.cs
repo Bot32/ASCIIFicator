e30219e035c3a275d881bc282e459ff4b796e05c
Мазур Константин <nepisatg@gmail.com>
2015-10-31 15:56:58 +0300

    V 1.0

---
+++ ASCIIficator/Converter.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ASCIIficator
+{
+    class Converter
+    {
+        //загрузка статической картинки
+        public static Bitmap LoadImage(Stream stream)
+        {
+            using (Image image = Image.FromStream(stream))
+            {
+                Bitmap bitmapImage = new Bitmap(image, new Size(image.Width, image.Height));
+                image.Dispose();
+                return bitmapImage;
+            }    
+        }
+
+        //конветация статической картинки
+        public static string ConvertImage(Bitmap bitmapImage, int sizeModifier)
+        {
+
+            StringBuilder asciiart = new StringBuilder();
+            Rectangle bounds = new Rectangle(0, 0, bitmapImage.Width, bitmapImage.Height);
+
+            #region greyscale image
+            ColorMatrix matrix = new ColorMatrix();
+
+            matrix[0, 0] = 1 / 3f;
+            matrix[0, 1] = 1 / 3f;
+            matrix[0, 2] = 1 / 3f;
+            matrix[1, 0] = 1 / 3f;
+            matrix[1, 1] = 1 / 3f;
+            matrix[1, 2] = 1 / 3f;
+            matrix[2, 0] = 1 / 3f;
+            matrix[2, 1] = 1 / 3f;
+            matrix[2, 2] = 1 / 3f;
+
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(matrix);
+
+            Graphics gphGrey = Graphics.FromImage(bitmapImage);
+            gphGrey.DrawImage(bitmapImage, bounds, 0, 0, bitmapImage.Width, bitmapImage.Height,
+                GraphicsUnit.Pixel, attributes);
+
+            gphGrey.Dispose();
+            #endregion
+
+            #region ascii image
+
+            int pixhight = sizeModifier * 2;
+            int pixseg = sizeModifier * pixhight;
+
+            for (int h = 0; h < bitmapImage.Height / pixhight; h++)
+            {
+                int startY = (h * pixhight);
+                for (int w = 0; w < bitmapImage.Width / sizeModifier; w++)
+                {
+                    int startX = (w * sizeModifier);
+                    int allBrightness = 0;
+
+                    for (int y = 0; y < sizeModifier; y++)
+                    {
+                        for (int x = 0; x < pixhight; x++)
+                        {
+                            int cY = y + startY;
+                            int cX = x + startX;
+                            try
+                            {
+                                Color c = bitmapImage.GetPixel(cX, cY);
+                                int b = (int)(c.GetBrightness() * 100);
+                                allBrightness = allBrightness + b;
+                            }
+                            catch
+                            {
+                                allBrightness = (allBrightness + 50);
+                            }
+                        }
+                    }
+
+                    int sb = (allBrightness / pixseg);
+                    if (sb < 10)
+                    {
+                        asciiart.Append("#");
+                    }
+                    else if (sb < 17)
+                    {
+                        asciiart.Append("@");
+                    }
+                    else if (sb < 24)
+                    {
+                        asciiart.Append("&");
+                    }
+                    else if (sb < 31)
+                    {
+                        asciiart.Append("0");
+                    }
+                    else if (sb < 38)
+                    {
+                        asciiart.Append("*");
+                    }
+                    else if (sb < 45)
+                    {
+                        asciiart.Append("|");
+                    }
+                    else if (sb < 52)
+                    {
+                        asciiart.Append("!");
+                    }
+                    else if (sb < 59)
+                    {
+                        asciiart.Append(";");
+                    }
+                    else if (sb < 66)
+                    {
+                        asciiart.Append(":");
+                    }
+                    else if (sb < 73)
+                    {
+                        asciiart.Append("'");
+                    }
+                    else if (sb < 80)
+                    {
+                        asciiart.Append("`");
+                    }
+                    else if (sb < 87)
+                    {
+                        asciiart.Append(".");
+                    }
+                    else
+                    {
+                        asciiart.Append(" ");
+                    }
+                }
+                asciiart.Append("\n");
+            }
+            #endregion
+
+            return asciiart.ToString();
+
+        }
+
+        //конвертация всего сета кадров анимации
+        public static void ConvertAnimation(List<ASCIIAnimationFrame> frames, int sizeModifier)
+        {
+            foreach(ASCIIAnimationFrame frame in frames)
+            {
+                frame.ConvertFrame(sizeModifier);
+            }
+        }
+
+        //загрузка и разбор по кадрам анимации
+        public static List<ASCIIAnimationFrame> GetASCIIAnimationFrames(Stream stream)
+        {
+            List<ASCIIAnimationFrame> frames = new List<ASCIIAnimationFrame>();
+            using (var gifImage = Image.FromStream(stream))
+            {
+                var dimension = new FrameDimension(gifImage.FrameDimensionsList[0]); 
+                int frameCount = gifImage.GetFrameCount(dimension); 
+                for (var i = 0; i < frameCount; i++)
+                {
+                    gifImage.SelectActiveFrame(dimension, i); 
+                    ASCIIAnimationFrame frame = new ASCIIAnimationFrame();
+                    frame.bmpFrame = (Bitmap)gifImage.Clone(); 
+                    frame.delay = gifImage.GetPropertyItem(20736).Value[i * 4] * 10;//получение задержки отображенич каждого кадра
+                    if (frame.delay == 0) frame.delay = 100;
+                    frames.Add(frame);
+                }
+                return frames;
+            }
+        }
+    }
+
+    //класс хранит данные одного кадра анимации
+    public class ASCIIAnimationFrame
+    {
+        public Bitmap bmpFrame; //неконвертированнвый кадр
+        public string asciiFrame; //конвертированныи кадр
+        public int delay; //задержка отображения кадра
+
+        //конвертирует текущий кадр
+        public void ConvertFrame(int sizeModifier)
+        {
+            asciiFrame = Converter.ConvertImage(bmpFrame, sizeModifier);
+        }
+    }
+}
---
+++ ASCIIficator/MainWindow.xaml.cs
@@ -0,0 +1,244 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Threading;
+using System.Threading;
+using System.IO;
+
+namespace ASCIIficator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Bitmap currImage; //загруженное неконвертированное изображение
+        string currASCIIImage; // текущее изображение, конвертированое в ascii
+        List<ASCIIAnimationFrame> currASCIIAnimation; //сет кадров текущей анимации
+        int sizeModifier = 2; // модификатор размера при конвертации; чем больше, тем мельче
+        bool stopFlag = false; //флаг для выхода из цикла проигрывания анимации
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //диалоговое окно загрузки картинки
+        private void load_button_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
+            NoImageButtonsBlock();
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open Image";
+            dlg.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            if (dlg.ShowDialog() == true)
+            {
+                LoadingActivity(true);
+                if (dlg.FileName.EndsWith("gif"))
+                {   
+                    LoadAndPlay(
+                        () => {
+                                currASCIIAnimation = Converter.GetASCIIAnimationFrames(dlg.OpenFile());
+                                Converter.ConvertAnimation(currASCIIAnimation, sizeModifier);
+                              },
+                        () => PlayASCIIAnimation(currASCIIAnimation));
+                }
+                else
+                {
+                    LoadAndPlay(
+                        () => {
+                                currImage = Converter.LoadImage(dlg.OpenFile());
+                                currASCIIImage = (Converter.ConvertImage(currImage, sizeModifier));
+                              },
+                        () =>  DrawASCIIImage(currASCIIImage));
+                }
+            }
+        }
+
+        //вывод статическое картинки
+        void DrawASCIIImage(String asciiImage)
+        {
+            stopFlag = false;
+            DispatcherAction((ThreadStart)(() =>
+            {
+                LoadingActivity(false);
+                textBlock.Text = asciiImage;
+            }));
+        }
+
+        //вывод анимированной картинки
+        void PlayASCIIAnimation(List<ASCIIAnimationFrame> animation)
+        {
+            stopFlag = false;
+            while (!stopFlag)
+            {
+                foreach (ASCIIAnimationFrame frame in animation)
+                {
+                    if (stopFlag) break;
+                    DispatcherAction((ThreadStart)(() =>
+                        {
+                            LoadingActivity(false);
+                            //DrawASCIIImage(frame.asciiFrame);   
+                            textBlock.Text = frame.asciiFrame;
+                        }));
+                    Thread.Sleep(frame.delay);
+                }
+            }
+            DispatcherAction((ThreadStart)(() =>
+            //richTextBox.Document.Blocks.Clear()
+            textBlock.Text = ""
+            ));
+        }
+
+        //увеличение картинки
+        private void incr_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sizeModifier > 1)
+            {
+                sizeModifier--;
+                ReDraw();
+                decr_button.IsEnabled = true;
+
+            } else
+            {
+                incr_button.IsEnabled = false;
+            }
+        }
+        //уменьшение картинки
+        private void decr_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sizeModifier < 10)
+            {
+                sizeModifier++;
+                ReDraw();
+                incr_button.IsEnabled = true;
+            } else
+            {
+                decr_button.IsEnabled = false;
+            }
+        }
+
+        //метод позволяет обратиться к интерфейсу из вторичных потоков
+        void DispatcherAction(Delegate action)
+        {
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, action);
+        }
+
+        //метод запускающий асинхронную задачу в отдельном потоке и продолжение к ней
+        //напр. - загрузка, конвертация картинки и продолжение - вывод картинки
+        void LoadAndPlay(Action first, Action second)
+        {
+            Task task = new Task(() => first.Invoke());
+            task.ContinueWith(t => second.Invoke());
+            task.Start();
+        }
+
+        //сброс всех полей и флагов
+        void Reset()
+        {
+            stopFlag = true;
+            textBlock.Text = "";
+            currImage = null;
+            currASCIIImage = null;
+            currASCIIAnimation = null;
+            sizeModifier = 2;
+        }
+
+        //блокировка кнопок во время загрузки и конвертации картинок
+        void LoadingActivity(bool activity)
+        {
+            load_button.IsEnabled = !activity;
+            incr_button.IsEnabled = !activity;
+            decr_button.IsEnabled = !activity;
+            save_button.IsEnabled = !activity;
+
+            if (activity) Task.Factory.StartNew(() => LoadingAnimation());
+        }
+        //блокировка кнопок когда картинка не загружена
+        void NoImageButtonsBlock()
+        {
+            incr_button.IsEnabled = false;
+            decr_button.IsEnabled = false;
+            save_button.IsEnabled = false;
+        }
+
+        //сообщения загрузки, чтобы понимать что приложение не висит
+        void LoadingAnimation()
+        {
+            while(stopFlag)
+            {
+                DispatcherAction((ThreadStart)(() =>
+                //richTextBox.AppendText("\n" + "Loading...")
+                textBlock.Text += "\n Loading..."
+                ));
+                Thread.Sleep(1000);
+            }
+        }
+
+        //перерисовка картинки
+        void ReDraw()
+        {
+            if (currImage != null)
+            {
+                stopFlag = true;
+                LoadingActivity(true);
+                LoadAndPlay(
+                     () => currASCIIImage = (Converter.ConvertImage(currImage, sizeModifier)),
+                     () => DrawASCIIImage(currASCIIImage));
+            }
+            if (currASCIIAnimation != null)
+            {
+                stopFlag = true;
+                LoadingActivity(true);
+                LoadAndPlay(
+                     () => Converter.ConvertAnimation(currASCIIAnimation, sizeModifier),
+                     () => PlayASCIIAnimation(currASCIIAnimation));
+            }
+        }
+
+        //диалоговое окно сохранения картинки
+        private void save_button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Title = "Save ASCII Image";
+            dlg.DefaultExt = ".txt";
+            dlg.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            if (dlg.ShowDialog() == true)
+            {
+                using (StreamWriter sw = new StreamWriter(dlg.OpenFile(), System.Text.Encoding.Default))
+                {
+                    SaveASCIIImage(sw);
+                }
+            }
+        }
+        void SaveASCIIImage(StreamWriter sw)
+        {
+            if (currASCIIImage != null)
+            {
+                foreach (string str in currASCIIImage.Split('\n'))
+                {
+                    sw.WriteLine(str);
+                }
+
+            };
+            if (currASCIIAnimation !=null)
+            {
+                foreach(ASCIIAnimationFrame frame in currASCIIAnimation)
+                {
+                    foreach(string str in frame.asciiFrame.Split('\n'))
+                    {
+                        sw.WriteLine(str);
+                    }
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            NoImageButtonsBlock();
+        }
+    }
+}
